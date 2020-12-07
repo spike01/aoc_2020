9ba89fb45f00833396c877b3f5b58e66136bf123
Spike <spike01@gmail.com>
2020-12-07 12:46:51 -0500

    Cleanup day7par2

--- day7/part2.go
+++ day7/part2.go
@@ -5,25 +5,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
-	"strings"
 )
-
-var count int
 
 type node struct {
 	color    string
-	contains map[string]*containment
+	contains map[string]*contents
 }
 
-type containment struct {
-	node *node
+type contents struct {
 	qty  int
+	node *node
 }
 
 func newNode(color string) *node {
-	return &node{color: color, contains: make(map[string]*containment)}
+	return &node{
+		color:    color,
+		contains: make(map[string]*contents),
+	}
 }
+
+var colorRegexp = regexp.MustCompile(`(\w+ \w+) bags contain`)
+var bagsRegexp = regexp.MustCompile(`(\d) (\w+ \w+)`)
 
 func main() {
 	f, err := os.Open("input.txt")
@@ -37,49 +41,49 @@ func main() {
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
 		next := sc.Text()
-		split := strings.Split(next, "bags")
-		split = strings.Split(strings.Join(split, " "), "contain")
-		color := split[0]
-		color = strings.TrimSpace(color)
-		node := newNode(color)
-		nodes[node.color] = node
-	}
-	nodes["o other"] = newNode("o other")
+		color := colorRegexp.FindStringSubmatch(next)[1]
 
-	f2, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatalf("Could not open file: %s", err)
-	}
-	defer f2.Close()
-	sc = bufio.NewScanner(f2)
-	for sc.Scan() {
-		next := sc.Text()
-		split := strings.Split(next, "bags")
-		split = strings.Split(strings.Join(split, " "), "contain")
-		color := split[0]
-		color = strings.TrimSpace(color)
-		contains := strings.ReplaceAll(split[1], ".", "")
-		containsSplit := strings.Split(contains, ",")
+		// Graph construction - don't nuke nodes if created as part of contents
+		node, ok := nodes[color]
+		if !ok {
+			node = newNode(color)
+			nodes[node.color] = node
+		}
 
-		for _, v := range containsSplit {
-			containsColor := v[2:]
-			containsColor = strings.ReplaceAll(containsColor, "bags", "")
-			containsColor = strings.ReplaceAll(containsColor, "bag", "")
-			containsColor = strings.TrimSpace(containsColor)
-			qty, _ := strconv.Atoi(string(v[1]))
-			node := nodes[color]
-			node.contains[containsColor] = &containment{qty: qty, node: nodes[containsColor]}
+		// regexp.MustCompile( `(\d) (\w+ \w+)`)
+		// Matches are in form:  0 (left-most)        -> 1    2 (capture groups)
+		//                     ((\d) (\w+ \w+  )  (\d) (\w+   w+)   )
+		//                      [(n color color), (n), (color color)]
+		containedBags := bagsRegexp.FindAllStringSubmatch(next, -1)
+
+		for _, v := range containedBags {
+			containedColor := v[2]
+
+			qty, err := strconv.Atoi(v[1])
+			if err != nil {
+				fmt.Println("Could not convert:", v[1], err)
+			}
+
+			// If node doesn't already exist, create it in the list
+			containedNode, ok := nodes[containedColor]
+			if !ok {
+				containedNode = newNode(containedColor)
+				nodes[containedNode.color] = containedNode
+			}
+
+			node.contains[containedColor] = &contents{qty: qty, node: nodes[containedColor]}
 		}
 	}
-	fmt.Println(nodes["shiny gold"])
 
-	recursiveSearch(nodes["shiny gold"], []int{1})
+	var count int
+	recursiveSearch(nodes["shiny gold"], []int{1}, &count)
 
 	fmt.Println("Count:", count)
 	os.Exit(0)
 }
 
-func recursiveSearch(n *node, prevLevel []int) {
+// Simple DFS search, whatever
+func recursiveSearch(n *node, prevLevel []int, count *int) {
 	if len(n.contains) == 0 {
 		return
 	}
@@ -90,8 +94,8 @@ func recursiveSearch(n *node, prevLevel []int) {
 			for _, n := range prevLevel {
 				mult = mult * n
 			}
-			count += v.qty * mult
+			*count += v.qty * mult
 		}
-		recursiveSearch(v.node, append(prevLevel, v.qty))
+		recursiveSearch(v.node, append(prevLevel, v.qty), count)
 	}
 }
