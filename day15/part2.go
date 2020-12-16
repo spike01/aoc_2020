46eb3378dd597b26fcc5f0f397e02e4e1d4f2226
Spike <spike01@gmail.com>
2020-12-15 23:15:09 -0500

    Day15 part2

---
+++ day15/part2.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatalf("Could not open file: %s", err)
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+
+	var starters []int
+
+	for sc.Scan() {
+		next := sc.Text()
+		split := strings.Split(next, ",")
+		for _, v := range split {
+			n, err := strconv.Atoi(v)
+			if err != nil {
+				log.Fatalf("Could not convert: %s", err)
+			}
+			starters = append(starters, n)
+		}
+	}
+
+	var next int
+
+	target := 30000000
+	turn := 1
+	numbers := make(map[int][]int)
+
+	for turn < len(starters) {
+		next = starters[turn-1]
+		numbers[next] = append(numbers[next], turn)
+		turn++
+	}
+
+	next = starters[len(starters)-1]
+
+	for turn < target {
+		positions, ok := numbers[next]
+
+		if !ok {
+			numbers[next] = append(numbers[next], turn)
+			next = 0
+			turn++
+			continue
+		}
+
+		lastTurn := positions[len(positions)-1]
+		numbers[next] = append(numbers[next], turn)
+
+		next = turn - lastTurn
+
+		turn++
+	}
+
+	fmt.Println("Turn", turn, ":", next)
+
+	os.Exit(0)
+}
